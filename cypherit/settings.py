9bf2a61c82a82a3147cce9de1f0137248c62526f
ragnar17 <edwardamit17@gmail.com>
2021-04-07 19:49:14 +0530

    Testing with posix path

--- cypherit/settings.py
+++ cypherit/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = ""
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,7 +86,7 @@ WSGI_APPLICATION = 'cypherit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
